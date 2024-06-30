bee6ab9ccca6e7946afcfd1aec1bdcbb6ea8fe74
hieu1966hn <hieu1966hn@gmail.com>
2024-06-30 16:00:46 +0700

    final code SS12

--- SS12/SS12.py
+++ SS12/SS12.py
@@ -7,7 +7,6 @@
 #     print(x)
 
 
-
 ##### Write to Existing File
 # f = open("./SS12/demofile.txt", "a")
 # f.write("Now the file has more content!")
@@ -31,10 +30,8 @@
 # print(f.read())
 
 
-
 ##### tạo một file nếu nó không tồn tại.
 # f = open("./SS12/demofile3.txt", "w")
-
 
 
 ##### Delete a file
@@ -52,8 +49,22 @@
 #### Xóa folder: os.rmdir()
 import os
 
-#### Tạo foder 
+#### Tạo foder
 # os.mkdir("./SS13") ### make directories
 
 #### Xóa foler
 # os.rmdir("./SS13") ### remove directories
+
+
+#### Chữa bài 3a
+f = open("./SS12/numbers.txt", "w")
+f.write("10\n20\n30\n40\n50")
+f.close()
+
+f = open("./SS12/numbers.txt", "r")
+total = 0
+for i in f:
+    # print(i)
+    i  = int(i)
+    total = total + i
+print(total)
