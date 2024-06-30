6b06fa74b8d8a6f6dd5b5f6771363f09d098f60a
hieu1966hn <hieu1966hn@gmail.com>
2024-06-30 15:34:05 +0700

    New code SS12

---
+++ SS12/SS12.py
@@ -0,0 +1,59 @@
+# f = open("./SS12/demofile.txt")
+# print(f.readline())
+# print(f.readline())
+
+### Dùng vòng lặp để line by line (giống readline())
+# for x in f:
+#     print(x)
+
+
+
+##### Write to Existing File
+# f = open("./SS12/demofile.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
+
+
+### Mở và đọc file sau khi thêm nội dung:
+# f = open("./SS12/demofile.txt", "r")
+# print(f.read())
+### Có open thì phải có close
+# f.close()
+
+
+##### Mở file demofile2.txt và ghi đè nội dung bên trong nó.
+# f = open("./SS12/demofile2.txt", "w")
+# f.write("Woop! I have deleted the content!")
+# f.close()
+
+### Mở file và đọc sau khi ghi đè file vừa rồi
+# f = open("./SS12/demofile2.txt", "r")
+# print(f.read())
+
+
+
+##### tạo một file nếu nó không tồn tại.
+# f = open("./SS12/demofile3.txt", "w")
+
+
+
+##### Delete a file
+# import os
+# os.remove("./SS12/demofile.txt")
+
+
+#### Ví dụ minh họa: kiểm tra file tồn tại, sau đó xóa nó.
+# import os
+# if os.path.exists("./SS12/demofile2.txt"):
+#     os.remove("./SS12/demofile2.txt")
+# else:
+#     print("The file does not exist")
+
+#### Xóa folder: os.rmdir()
+import os
+
+#### Tạo foder 
+# os.mkdir("./SS13") ### make directories
+
+#### Xóa foler
+# os.rmdir("./SS13") ### remove directories
---
+++ SS12/SS12_lythuyet.md
@@ -0,0 +1,22 @@
+Buổi 12: Đọc ghi File.
+- File: là đơn vị lưu trữ thông tin trong hệ thống máy tính. File có thể chứa nhiều loại dữ liệu khác nhau gồm: video, hình ảnh, văn bản, âm thanh, ... Các file được tổ chức và lưu trữ trong các thư mục (folder).
+- Liệt kê các đuôi file cơ bản: 
++ .mp3: file âm thanh
++ .mp4: file video
++ .doc/.pdf/.pptx/: tài liệu
++ .xlxs: file bảng tính.
++ .psd: .....
++ .exe: file thực thi 
+
+
+Khái niệm: File path
+
+r: read only, Báo lỗi nếu không có file
+a: Thêm nội dung vào cuối file, Tạo file nếu chưa có file đó
+w: Ghi đè file, tạo file nếu chưa có file đó
+x: Tạo file
+
+Xóa file: 
+- import module os
+- hàm remove
+- hàm rmdir()
